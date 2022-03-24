8e9dd6770601c4f503105116ed54e2a5a97bdf63
Alana Vasconcelos <alanamvd30@gmail.com>
2022-03-24 07:44:16 -0300

    Linkando o js no html

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,46 @@ const listaSelecaoPokemon = document.querySelectorAll('.pokemon')
 const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
 
 listaSelecaoPokemon.forEach(pokemon => {
+
+
+    pokemon.addEventListener('click', () => {
+
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
